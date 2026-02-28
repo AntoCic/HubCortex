2a4c8f6b4b18b4418a02d99bc7fd2e163ebea8a7
AntoCic <anto.cic.127@gmail.com>
2026-02-28 15:30:23 +0100

    v0.0.6 – b

--- functions/src/config/secret.ts
+++ functions/src/config/secret.ts
@@ -1,5 +1,9 @@
 // functions/src/config/secret.ts
 
+import { defineSecret } from 'firebase-functions/params';
 export const secret = {
     GEMINI_API_KEY: 'GEMINI_API_KEY',
 } as const;
+
+
+export const GEMINI_API_KEY = defineSecret('GEMINI_API_KEY');
